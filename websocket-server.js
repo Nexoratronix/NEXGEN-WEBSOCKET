059b04e5d453d4bbc4272077f945be75c9ab4fc7
Nexoratronix <dev@nexoratronix.com>
2025-04-08 14:58:49 +0530

    update vercel path

--- websocket-server.js
+++ websocket-server.js
@@ -6,7 +6,7 @@ const { createAdapter } = require("@socket.io/redis-adapter");
 const server = createServer();
 const io = new Server(server, {
   cors: {
-    origin: process.env.NEXTJS_APP_URL, // Your Vercel app URL
+    origin: "https://nexgen-staging.vercel.app", // Your Vercel app URL
     methods: ["GET", "POST"],
   },
 });
